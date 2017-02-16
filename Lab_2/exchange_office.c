4763db965f30c5be9d5208829c9ffe12d9eee771
sahil-mehra <sm.sahilmehra@gmail.com>
2017-02-16 04:22:11 +0000

    exchange_office.c was added

---
+++ Lab_2/exchange_office.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int eur;
+void choice();
+
+int main() {
+  printf("Greetings, welcome to the Currency Exchange. Here are the rates:\n");
+  printf("EURO\t USD\t   GBP\n");
+  for(int eur = 1; eur < 11; ++eur){
+    printf("%d",eur);
+    printf("\t %.3f", 1.293 * eur);
+    printf("\t   %.3f", 0.693 * eur);
+    printf("\n");
+  }
+  printf("\n\n\n");
+  choice();
+
+  return 0;
+}
+
+void choice() {
+  printf("Enter 1 to convert from EURO to USD.\n");
+  printf("Enter 2 to convert from EURO to GBP.\n");
+  int choice1;
+  printf("Choice: ");
+  scanf("%d", &choice1);
+  printf("\n");
+  switch(choice1) {
+    case 1:
+      printf("You've picked EURO to USD\n");
+      printf("Please enter amount to be converted: ");
+      scanf("%d", &eur);
+      printf("That is $ %f", 1.293 * eur);
+      printf("\n");
+      break;
+    case 2:
+      printf("You've picked EURO to GBP\n");
+      printf("Please enter amount to be converted: ");
+      scanf("%d", &eur);
+      printf("That is $ %f", .693 * eur);
+      printf("\n");
+      break;
+    default:
+      printf("You have made an illegal choice.\n");
+      printf("Please enter available choice.\n\n\n");
+      choice();
+    }
+}
