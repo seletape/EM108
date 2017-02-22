da9bc21fade1abe6a2711fe0dbe2b21a5e32f84e
sahil-mehra <sm.sahilmehra@gmail.com>
2017-02-22 17:32:17 +0000

    Finished coding 3_number_calculation.c and started n_number_calculations.c

--- Lab_3/3_number_caclulation.c
+++ Lab_3/3_number_caclulation.c
@@ -8,7 +8,7 @@ float z;
 void input();
 void calculations();
 void maximum();
-void minimun();
+void minimum();
 
 int main() {
   printf("Welcome to the the Three Number Calculator\n");
@@ -39,21 +39,21 @@ void input() {
 
 //The calculations for sum, product and average
 void calculations() {
-  printf("The sum of the three numbers is: %f\n", x + y + z);
-  printf("The product of the three numbers is: %f\n", x * y * z);
-  printf("The average of the three numbers is: %f\n", (x + y + z)/3 );
+  printf("The sum of the three numbers is: %.3f\n", x + y + z);
+  printf("The product of the three numbers is: %.3f\n", x * y * z);
+  printf("The average of the three numbers is: %.3f\n", (x + y + z)/3 );
 }
 
 //The largest number of all three
 void maximum() {
-  if((x >= y) || (x >= z)) {
-    printf("The maximum is: %f\n", x);
+  if((x >= y) && (x >= z)) {
+    printf("The maximum is: %.3f\n", x);
   }
-  else if((y >= x) || (y >= z)) {
-    printf("The maximum is: %f\n", y);
+  else if((y >= x) && (y >= z)) {
+    printf("The maximum is: %.3f\n", y);
   }
-  else if((z >= x) || (z >= y)) {
-    printf("The maximum is: %f\n", z);
+  else if((z >= x) && (z >= y)) {
+    printf("The maximum is: %.3f\n", z);
   }
   else {
     printf("The numbers equal each other.\n");
@@ -62,14 +62,14 @@ void maximum() {
 
 //The smallest number of all three
 void minimum() {
-  if((x <= y) || (x <= z)) {
-    printf("The minimum is: %f\n", x);
+  if((x <= y) && (x <= z)) {
+    printf("The minimum is: %.3f\n", x);
   }
-  else if((y <= x) || (y <= z)) {
-    printf("The minimum is: %f\n", y);
+  else if((y <= x) && (y <= z)) {
+    printf("The minimum is: %.3f\n", y);
   }
-  else if((z <= x) || (z <= y)) {
-    printf("The minimum is: %f\n", z);
+  else if((z <= x) && (z <= y)) {
+    printf("The minimum is: %.3f\n", z);
   }
   else {
     printf("The numbers equal each other.\n");
--- Lab_3/n_number_calculation.c
+++ Lab_3/n_number_calculation.c
@@ -4,7 +4,21 @@
 float n;
 
 void input();
+void calculations();
 
 int main() {
+  printf("Welcome to the Calculator.");
+  input();
+  calculations();
 
+}
+
+void input() {
+  printf("How many numbers would you like to enter to the calculator?");
+  scanf("%f", &n);
+  printf("\n");
+}
+
+void calculations() {
+  for(n)
 }
